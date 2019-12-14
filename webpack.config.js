111efd7cfa3de80264c98c7fd03745f4b48c6588
Dennis <mealthebear@gmail.com>
2019-12-14 15:59:07 -0800

    Reconfigured webpack to build faster

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/public');
 
 module.exports = {
-    entry: "./client/src/index.js",
-    output: {
-        path: path.resolve(__dirname, './client/public'),
-        filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          },
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
