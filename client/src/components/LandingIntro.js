04b997e5a231900d68d36d52dc237f762147e7a3
Dennis <mealthebear@gmail.com>
2019-12-15 17:56:19 -0800

    Set up introductory text on the home page

--- client/src/components/LandingIntro.js
+++ client/src/components/LandingIntro.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 const LandingIntro = (props) => (
-    <div>
-        <div>
+    <div id="landing-container">
+        <div className="crest-container">
             <img src="https://www.mistyteahouse.com/wp-content/uploads/2015/12/Monk-Crest-Vector.png?w=640" alt="Monk Crest" className="monk-crest"/>
+        </div>
+        <div className="intro-container">
+            <p className="hello">Hello, I'm</p>
+            <p className="peimei">Peimei</p>
         </div>
     </div>
 )
