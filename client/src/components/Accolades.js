9e5f5e8bb0f2643b67221145ea5c3df805cf69c2
Dennis <mealthebear@gmail.com>
2019-12-16 14:27:12 -0800

    Implemented spacing for accomplishment list items

--- client/src/components/Accolades.js
+++ client/src/components/Accolades.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 const Accolades = (props) => (
     <div className="accolades-container">
         <div className="accolades-text">Notable Accomplishments of My Monk Career</div>
-        <div><span className="number-circle">1</span><p>Saved a bunch of dweebs from the brink of death during quaking</p></div>
-        <div><span className="number-circle">2</span><p>Did more damage than the tank during boss fights (and it wasn't even shrine!)</p></div>
-        <div><span className="number-circle">3</span><p>Told the tank to run the $%#@ away and drop necrotic stacks, single-handedly saving the key</p></div>
+            <div><span className="number-circle">1</span><p>Saved a bunch of dweebs from the brink of death during quaking</p></div>
+            <div><span className="number-circle">2</span><p>Did more damage than the tank during boss fights (and it wasn't even shrine!)</p></div>
+            <div><span className="number-circle">3</span><p>Told the tank to run the $%#@ away and drop necrotic stacks, single-handedly saving the key</p></div>
     </div>
 )
 
