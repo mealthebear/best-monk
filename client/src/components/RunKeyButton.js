8342f87c40f08f18c51d2a183affd7fd7da91347
Dennis <mealthebear@gmail.com>
2019-12-15 18:40:18 -0800

    Centered button and added styling

--- client/src/components/RunKeyButton.js
+++ client/src/components/RunKeyButton.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 const RunKeyButton = (props) => (
-    <div className="run-key">
-        Wanna Run A Key?
+    <div className="run-key-container">
+        <div className="run-key">Wanna Run A Key?</div>
     </div>
 )
 
