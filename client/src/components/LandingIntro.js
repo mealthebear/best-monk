7e658759621928d600cc57c0ddaec9042fb5c62a
Dennis <mealthebear@gmail.com>
2019-12-15 17:06:02 -0800

    Inserted place holder image

--- client/src/components/LandingIntro.js
+++ client/src/components/LandingIntro.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 const LandingIntro = (props) => (
     <div>
-        
+        <div>
+            <img src="https://www.mistyteahouse.com/wp-content/uploads/2015/12/Monk-Crest-Vector.png?w=640" alt="Monk Crest" className="monk-crest"/>
+        </div>
     </div>
 )
 
