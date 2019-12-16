ca37588b98a97687d5ed2c1b5421a29713b8aa3d
Dennis <mealthebear@gmail.com>
2019-12-16 13:01:55 -0800

    Created component for showing character accolades

---
+++ client/src/components/Accolades.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Accolades = (props) => (
+    <div className="accolades-container">
+        
+    </div>
+)
+
+export default Accolades;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar.js';
 import LandingIntro from './LandingIntro.js';
+import Accolades from './Accolades.js';
 
 export default class App extends Component {
     constructor(props) {
@@ -15,6 +16,7 @@ export default class App extends Component {
                     <Navbar />
                 </header>
                 <LandingIntro />
+                <Accolades />
             </div>
         )
     }
