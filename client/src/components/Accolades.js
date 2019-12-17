0a95a6925436c9d4b1943b27b3e6fabbed335ebb
Dennis <mealthebear@gmail.com>
2019-12-17 11:55:38 -0800

    Changed capitalization of a word

--- client/src/components/Accolades.js
+++ client/src/components/Accolades.js
@@ -7,7 +7,7 @@ const Accolades = (props) => (
             <hr className="line-1"/>
             <span className="number-circle nc-2">2</span><p className="achieve-2">Outputted some sick fist-weaving DPS on boss fights (and it wasn't even <strong className="shrine">Shrine!</strong>)</p>
             <hr className="line-2"/>
-            <span className="number-circle nc-3">3</span><p className="achieve-3">Told the tank to run the $%#@ away and drop necrotic stacks, single-handedly saving the key</p>
+            <span className="number-circle nc-3">3</span><p className="achieve-3">Told the tank to run the $%#@ away and drop Necrotic stacks, single-handedly saving the key</p>
     </div>
 )
 
