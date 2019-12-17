0b3a08301d3c09693597b21b237992712b205cbd
Dennis <mealthebear@gmail.com>
2019-12-16 19:18:43 -0800

    Made minor text changes in personal accolades

--- client/src/components/Accolades.js
+++ client/src/components/Accolades.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const Accolades = (props) => (
     <div className="accolades-container">
         <div className="accolades-text">Notable Accomplishments of My Monk Career</div>
-            <span className="number-circle">1</span><p>Saved a bunch of dweebs from the brink of death during quaking and bursting</p>
+            <span className="number-circle">1</span><p>Saved a bunch of dweebs from the brink of death during Quaking and Bursting</p>
             <hr className="line-1"/>
             <span className="number-circle">2</span><p>Outputted some sick fist-weaving DPS on boss fights (and it wasn't even Shrine!)</p>
             <hr className="line-2"/>
