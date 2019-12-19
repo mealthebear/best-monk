dc5eae11213a5eb85679fff08c25c28e3b393d9e
Dennis <mealthebear@gmail.com>
2019-12-18 19:35:40 -0800

    Added christmas colors finish to holiday text

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -9,7 +9,16 @@ export default class Contact extends Component {
     render() {
         return (
             <div id="contact">
-                <p>Aw shucks! It looks like I'm all booked for the holidays.</p>
+                <p>Aw shucks! It looks like I'm all booked for the 
+                    <span> </span>
+                    <span className="red">H</span>
+                    <span className="red">o</span>
+                    <span className="red">l</span>
+                    <span className="red">i</span>
+                    <span className="green">d</span>
+                    <span className="green">a</span>
+                    <span className="green">y</span>
+                    <span className="green">s</span>.</p>
                 <p>Check back again soon!</p>
             </div>
         )
