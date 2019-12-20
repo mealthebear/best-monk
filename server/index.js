4a84b3516678b54f94a30f0bd04d3719f4a61638
Dennis <mealthebear@gmail.com>
2019-12-20 11:28:04 -0800

    Added endpoint to use router

--- server/index.js
+++ server/index.js
@@ -10,4 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/public')));
 
+app.use('/api', router);
+
 app.listen(port, () => console.log(`Listening on port ${port} yay!`));
