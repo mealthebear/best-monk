2ced03eba0a09836ee6c00cc02049ec87354db56
Dennis <mealthebear@gmail.com>
2020-01-01 15:57:42 -0800

    Added inputs and labels to form

--- client/src/components/contact/KeyForm.js
+++ client/src/components/contact/KeyForm.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const KeyForm = (props) => (
     <div>
-
+        <form id="keyform">
+            <label>Character Name</label>
+            <input type="text" name="charname" />
+            <label>Realm</label>
+            <input type="text" name="realm" />
+            <label>Dungeon</label>
+            <input type="text" name="dungeon" />
+            <label>Level</label>
+            <input type="number" name="dungeon" />
+        </form>
     </div>
 )
 
