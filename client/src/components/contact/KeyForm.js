3965329fcfd86fd476d5a3396d53da9f4cdeb87a
Dennis <mealthebear@gmail.com>
2020-01-02 14:58:13 -0800

    Styled submit button and made inputs required

--- client/src/components/contact/KeyForm.js
+++ client/src/components/contact/KeyForm.js
@@ -4,13 +4,13 @@ const KeyForm = (props) => (
     <div>
         <form id="keyform" onSubmit={props.onSubmit}>
             <label>Character Name</label>
-            <input className="key-input" type="text" name="charName" onChange={props.onChange} />
+            <input className="key-input" type="text" name="charName" required onChange={props.onChange} />
             <label>Realm</label>
-            <input className="key-input" type="text" name="realm" onChange={props.onChange} />
+            <input className="key-input" type="text" name="realm" required onChange={props.onChange} />
             <label>Dungeon</label>
-            <input className="key-input" type="text" name="dungeon" onChange={props.onChange} />
+            <input className="key-input" type="text" name="dungeon" required onChange={props.onChange} />
             <label>Level</label>
-            <input id="level" className="key-input" type="number" name="level" onChange={props.onChange} />
+            <input id="level" className="key-input" type="number" name="level" required onChange={props.onChange} />
             {/* <input className="key-submit" type="submit" value="Submit" /> */}
             <div></div>
             <button className="key-submit">Submit</button>
