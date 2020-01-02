25e1d137e63100da640ce18a1bbad6db3871ed73
Dennis <mealthebear@gmail.com>
2020-01-02 13:01:45 -0800

    Created handler to register user input

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -5,14 +5,27 @@ import KeyForm from './KeyForm.js';
 export default class Contact extends Component {
     constructor(props) {
         super(props)
-        this.state = {};
+        this.state = {
+            charName: '',
+            realm: '',
+            dungeon: '',
+            level: 0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
     }
 
     render() {
         return (
             <div id="contact">
                 {/* <FullyBooked /> */}
-                <KeyForm />
+                <KeyForm onChange={this.handleChange} />
             </div>
         )
     }
--- client/src/components/contact/KeyForm.js
+++ client/src/components/contact/KeyForm.js
@@ -4,13 +4,13 @@ const KeyForm = (props) => (
     <div>
         <form id="keyform">
             <label>Character Name</label>
-            <input className="key-input" type="text" name="charname" />
+            <input className="key-input" type="text" name="charName" onChange={props.onChange} />
             <label>Realm</label>
-            <input className="key-input" type="text" name="realm" />
+            <input className="key-input" type="text" name="realm" onChange={props.onChange} />
             <label>Dungeon</label>
-            <input className="key-input" type="text" name="dungeon" />
+            <input className="key-input" type="text" name="dungeon" onChange={props.onChange} />
             <label>Level</label>
-            <input id="level" className="key-input" type="number" name="dungeon" />
+            <input id="level" className="key-input" type="number" name="level" onChange={props.onChange} />
             <input className="key-submit" type="submit" value="Submit" />
         </form>
     </div>
