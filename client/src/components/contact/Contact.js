080d6cccd57dbb29c4d50b102995e31ee9094dca
Dennis <mealthebear@gmail.com>
2020-01-02 13:15:53 -0800

    Implemented function to post user

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FullyBooked from './FullyBooked.js';
 import KeyForm from './KeyForm.js';
+import Axios from 'axios';
 
 export default class Contact extends Component {
     constructor(props) {
@@ -13,11 +14,32 @@ export default class Contact extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.postUser = this.postUser.bind(this);
     }
 
     handleChange(e) {
         this.setState({
             [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit() {
+        this.postUser();
+    }
+
+    postUser() {
+        Axios.post('/api/key', {
+            charName: this.state.charName,
+            realm: this.state.realm,
+            dungeon: this.state.dungeon,
+            level: this.state.level
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
         })
     }
 
