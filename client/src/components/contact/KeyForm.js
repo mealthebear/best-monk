fe2af7f2030c13606ba276ec908c818e014a8d43
Dennis <mealthebear@gmail.com>
2020-01-09 15:40:49 -0800

    Gave form elements unique identifiers

--- client/src/components/contact/KeyForm.js
+++ client/src/components/contact/KeyForm.js
@@ -3,17 +3,17 @@ import React from 'react';
 const KeyForm = (props) => (
     <div>
         <form id="keyform" onSubmit={props.onSubmit}>
-            <label>Character Name</label>
-            <input className="key-input" type="text" name="charName" required onChange={props.onChange} />
-            <label>Realm</label>
-            <input className="key-input" type="text" name="realm" required onChange={props.onChange} />
-            <label>Dungeon</label>
-            <input className="key-input" type="text" name="dungeon" required onChange={props.onChange} />
-            <label>Level</label>
-            <input id="level" className="key-input" type="number" name="level" required onChange={props.onChange} />
+            <label id="char-name-label">Character Name</label>
+            <input id="char-name-input" className="key-input" type="text" name="charName" required onChange={props.onChange} />
+            <label id="realm-label">Realm</label>
+            <input id="realm-input" className="key-input" type="text" name="realm" required onChange={props.onChange} />
+            <label id="dungeon-label">Dungeon</label>
+            <input id="dungeon-input" className="key-input" type="text" name="dungeon" required onChange={props.onChange} />
+            <label id="level-label">Level</label>
+            <input id="level-input" className="key-input" type="number" name="level" required onChange={props.onChange} />
             {/* <input className="key-submit" type="submit" value="Submit" /> */}
             <div></div>
-            <button className="key-submit">Submit</button>
+            <button id="key-submit">Submit</button>
         </form>
     </div>
 )
