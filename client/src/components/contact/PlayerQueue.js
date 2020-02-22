b3170046829024f575e496e16762daf9855f3063
Dennis <mealthebear@gmail.com>
2020-02-21 22:06:44 -0800

    Implemented function to fetch all players

--- client/src/components/contact/PlayerQueue.js
+++ client/src/components/contact/PlayerQueue.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class PlayerQueue extends Component {
     constructor(props) {
         super(props)
         this.state = {};
+        this.getPlayers = this.getPlayers.bind(this);
+    }
+
+    getPlayers() {
+        Axios.get('/api/key')
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
     }
 
     render() {
         return (
             <div id="queue-section">
-                <div id="queue">
+                <div id="queue" onClick={this.getPlayers}>
                     Show the Queue
                 </div>
             </div>
