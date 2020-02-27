453c511c0e6bb14e49307a80d8c4abf107dc7610
Dennis <mealthebear@gmail.com>
2020-02-26 22:39:16 -0800

    Changed format of how queue info is displayed

--- client/src/components/contact/PlayerQueueList.js
+++ client/src/components/contact/PlayerQueueList.js
@@ -4,9 +4,8 @@ const PlayerQueueList = (props) => (
     <div>
         {props.players.map((player, i) => (
             <div className="player-box" key={i}>
-                <p>Name: {player.charName}</p>
-                <p>Realm: {player.realm}</p>
-                <p>Key: {player.dungeon} {player.level}</p>
+                <p>Character: {player.charName}-{player.realm}</p>
+                <p>Key: [{player.dungeon} ({player.level})]</p>
             </div>
         ))}
     </div>
