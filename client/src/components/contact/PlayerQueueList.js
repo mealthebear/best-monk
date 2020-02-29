d9647b74fdbc645fe0c2ff9e01c4f60b429fe187
Dennis <mealthebear@gmail.com>
2020-02-28 22:10:02 -0800

    Implemented styling for character names in queue

--- client/src/components/contact/PlayerQueueList.js
+++ client/src/components/contact/PlayerQueueList.js
@@ -4,7 +4,7 @@ const PlayerQueueList = (props) => (
     <div>
         {props.players.map((player, i) => (
             <div className="player-box" key={i}>
-                <p>Character: {player.charName}-{player.realm}</p>
+                <p className="player-name">Character: {player.charName}-{player.realm}</p>
                 <p>Key: <span className="key-name">[{player.dungeon} ({player.level})]</span></p>
             </div>
         ))}
