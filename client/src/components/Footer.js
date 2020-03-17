d873679d3046eff3643856ce31df85981270f090
Dennis <mealthebear@gmail.com>
2020-03-16 23:56:37 -0700

    Changed footer message

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const Footer = (props) => (
     <div className="footer">
-        <span>What kind of key do you take me for?</span> <span>(✿ ♥‿♥) Made With 💙 (✿ ♥‿♥)</span>
+        <span>Hip hip, hooray! It's key day!</span> <span>(✿ ♥‿♥) Made With 💙 (✿ ♥‿♥)</span>
     </div>
 )
 
